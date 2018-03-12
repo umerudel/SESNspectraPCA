b9c6b66667b88d65cb2fa543042d005fa30eaece
Marc Williamson <mew488@PD326.PHYSICS.NYU.EDU>
2018-03-11 21:54:58 -0400

    finished implementing SNspecFFTsmooth in python. Added to NovaPCA.py. Need to change preprocess/gapMask.

--- NovaPCA.py
+++ NovaPCA.py
@@ -3,6 +3,9 @@
 # Date created: 3/01/2018
 
 import numpy as np
+import scipy
+import scipy.optimize as opt
+import scipy.stats as st
 import matplotlib.pyplot as plt
 import sys, os, glob, re, copy
 import sklearn
@@ -64,6 +67,77 @@ def getSNeTypeMasks(sneTypes):
     IcMask = np.logical_or(tmp2, np.array([np.array_equal(typeArr,[3,3]) for typeArr in sneTypes]))
 
     return IIbMask, IbMask, IcMask, IcBLMask
+
+# Binspec implemented in python.
+def binspec(wvl, flux, wstart, wend, wbin):
+    nlam = (wend - wstart) / wbin + 1
+    nlam = int(np.ceil(nlam))
+    outlam = np.arange(nlam) * wbin + wstart
+    answer = np.zeros(nlam) 
+    interplam = np.unique(np.concatenate((wvl, outlam)))  
+    interpflux = np.interp(interplam, wvl, flux)
+    
+    for i in np.arange(0, nlam - 1):
+        cond = np.logical_and(interplam >= outlam[i], interplam <= outlam[i+1])
+        w = np.where(cond)
+        if len(w) == 2:
+            answer[i] = 0.5*(np.sum(interpflux[cond])*wbin)
+        else:
+            answer[i] = scipy.integrate.simps(interpflux[cond], interplam[cond])
+
+    answer[nlam - 1] = answer[nlam - 2]
+    cond = np.logical_or(outlam >= max(wvl), outlam < min(wvl))
+    answer[cond] = 0
+    return answer/wbin, outlam
+
+#smooth spectrum using SNspecFFTsmooth procedure
+def smooth(wvl, flux, cut_vel):
+    c_kms = 299792.47 # speed of light in km/s
+    vel_toolarge = 100000 # km/s
+    
+    wvl_ln = np.log(wvl)
+    num = wvl_ln.shape[0]
+    binsize = wvl_ln[-1] - wvl_ln[-2]
+    f_bin, wln_bin = binspec(wvl_ln, flux, min(wvl_ln), max(wvl_ln), binsize)
+    fbin_ft = np.fft.fft(f_bin)*len(f_bin)
+    freq = np.fft.fftfreq(num)
+    num_upper = np.max(np.where(1.0/freq[1:] * c_kms * binsize > cut_vel))
+    num_lower = np.max(np.where(1.0/freq[1:] * c_kms * binsize > vel_toolarge))
+    mag_avg = np.mean(np.abs(fbin_ft[num_lower:num_upper+1]))
+    powerlaw = lambda x, amp, exp: amp*x**exp
+    
+    #do linear regression on log data to obtain a guess for powerlaw parameters
+    xdat = freq[num_lower:num_upper]
+    ydat = np.abs(fbin_ft[num_lower:num_upper])
+    nonzero_mask = xdat!=0            
+    slope, intercept, _,_,_ = st.linregress(np.log(xdat[nonzero_mask]), np.log(ydat[nonzero_mask]))
+    exp_guess = slope
+    amp_guess = np.exp(intercept)
+    
+    #do powerlaw fit
+    xdat = freq[num_lower:num_upper]
+    ydat = np.abs(fbin_ft[num_lower:num_upper])
+    #exclude data where x=0 because this can cause 1/0 errors if exp < 0
+    finite_mask = np.logical_not(xdat==0)
+    finite_mask = np.logical_and(finite_mask, np.isfinite(ydat))
+    ampfit, expfit = opt.curve_fit(powerlaw, xdat[finite_mask], ydat[finite_mask], p0=[amp_guess, exp_guess])[0]
+
+    #find intersection of average fbin_ft magnitude and powerlaw fit to calculate separation
+    #velocity between signal and noise.
+    intersect_x = np.power((mag_avg/ampfit), 1.0/expfit)
+    sep_vel = 1.0/intersect_x * c_kms * binsize
+    
+    #filter out frequencies with velocities higher than sep_vel
+    smooth_fbin_ft = np.array([fbin_ft[ind] if np.abs(freq[ind])<np.abs(intersect_x) else 0 \
+                               for ind in range(len(freq))])/len(f_bin)
+    #inverse fft on smoothed fluxes
+    smooth_fbin_ft_inv = np.real(np.fft.ifft(smooth_fbin_ft))
+    
+    #interpolate smoothed fluxes back onto original wavelengths
+    w_smoothed = np.exp(wln_bin)
+    f_smoothed = np.interp(wvl, w_smoothed, smooth_fbin_ft_inv)
+    
+    return w_smoothed, f_smoothed, sep_vel
 
 
 
@@ -246,6 +320,31 @@ SNID Type Structure:
         self.phaseCols = np.copy(phaseCols)
 
         return
+
+
+    def smoothSpectra(self, mask, vel_cut, figsize):
+        f = plt.figure(figsize=figsize)
+        nshow = np.sum(mask)
+        plotcounter = 1
+        for i in range(len(mask)):
+            if mask[i]:
+                wsmooth, fsmooth, sepvel = smooth(self.wavelengths, self.spectraMatrix[i], vel_cut)
+                plt.subplot(nshow, 1, plotcounter)
+                plotcounter = plotcounter + 1
+                name = self.sneNames[i]
+                plt.plot(self.wavelengths, self.spectraMatrix[i], label='pre-smoothed '+name, color='r')
+                plt.plot(self.wavelengths, fsmooth, label='smoothed sep_vel = '+str(sepvel), color='k')
+                if i==0:
+                    plt.title('Smoothed SNe Spectra %d$\pm$%d'%(self.loadPhase, self.phaseWidth))
+                if i == nshow - 1:
+                    plt.xlabel("Wavelength (Angstroms)")
+                plt.ylabel('Rel Flux')
+                plt.legend(fontsize=12)
+                self.spectraMatrix[i] = fsmooth
+
+        return f
+
+
 
 # This Method smooths the broadline Ic spectra and outputs
 # a plot of the smoothed vs unsmoothed spectra for verification.
