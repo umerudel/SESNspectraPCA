8ddcb54a61024a75ffa812a348e7ede286f456e9
Umer Zubair <umer@udel.edu>
2019-10-04 00:04:43 -0400

    added lowres_dataset function

--- code/BinSpectra.py
+++ code/BinSpectra.py
@@ -187,7 +187,11 @@ def binspec_right(wvl, flux, uncer, bin_factor):
 # Drops bins from left.
 
 def mybinspec(wvl, flux, uncer, wbin):
-    owbin = (wvl[1] - wvl[0])
+    data_dtype = flux.dtype
+    flux = flux.astype('float64')
+    phase_key = list(uncer.keys())[0]
+    uncer = uncer[phase_key]
+    #wbin = (wvl[1] - wvl[0])
     #nwbin = obl * binbfactor
     bin_factor = float(wbin) / owbin
     wvl_log = np.log(wvl)
@@ -198,8 +202,6 @@ def mybinspec(wvl, flux, uncer, wbin):
         N = bin_factor
     N1 = int(N)
     N3 = int(N)
-    #n = N - N1
-    #n0 = N - N1
     #######################################################################################################
     integer_number_of_new_bins = int(len(wvl_log) / N)                                                    #
     used_bins = integer_number_of_new_bins * N                                                            #
@@ -224,25 +226,32 @@ def mybinspec(wvl, flux, uncer, wbin):
     new_uncer = []
     new_bin_center = wvl_log[i] + bin_radius_diff + remove_bin_fraction * old_bin_length
     new_wvl.append(np.exp(new_bin_center))
-    #print("unpb:", unpairedbins, "remove_bins:", remove_bins, "N1_skip:", N1_skip, "n_new:", n_new, "n_new_2:", n_new_2,               #
-     #     "SUM:", n_new+n_new_2, "i:", i, "N1_new:", N1_new, "i + N1_new:", i+N1_new, "BINS:" "brdiff:", brdiff, "wvl[-1]:", wvl[-1], "LAST BIN:", wvl[-1] - brdiff)     
     new_flux.append(n_new * flux[i] + flux[i + 1:i + N1_new].sum() + n_new_2 * flux[i + N1_new]) 
     new_uncer.append(np.sqrt(np.square(n_new * uncer[i]) + np.sum(np.square(uncer[i + 1:i + N1_new])) + np.square(n_new_2 * uncer[i + N1_new])))
     i = i + N1_new - N1
     while i + N1 + (int(N - 1.0000000000001 + n_new_2) + 1) <= len(wvl_log) - 1:
-        #print(i + N1 + (int(N - 1.0000000000001 + n_new_2) + 1), "i:", i, "N3:", N3, "N1:", N1, 
-        #      "n_new_0:", n_new_0, "n_new_2:", n_new_2, "i + N1:", i+N1, "n_test:", n_test, "N1_new:", N1_new)
-        #print("OLD =>" "n_new_2:", n_new_2, "n_new_0:", n_new_0, "i:", i, "i + N1:", i + N1 + (int(N - 1 + n_new_2) + 1))
         i = i + N1
         k = k + N3
         N1 = int(N - 1.0000000000001 + n_new_2) + 1
         N3 = int(N - 1.0000000000001 - n_new_0) + 1
         n_new_0 = N - N1 + n_new_0
-        #print("n_new_2:",1 - n_new_2, "N_NEW_0:", n_new_0, "i:", i, "i + N1:", i + N1, "N1:", N1)
         new_bin_center +=  (1 - n_new_2) * (wvl_log[i+1] - wvl_log[i])  + wvl_log[i + N1] - wvl_log[i + 1] + n_new_0 * (wvl_log[i + N1] - wvl_log[i + N1 - 1])
         new_wvl.append(np.exp(new_bin_center))
         new_flux.append((1 - n_new_2) * flux[i] + flux[i + 1:i + N1].sum() + n_new_0 * flux[i + N1])
         new_uncer.append(np.sqrt(np.square((1 - n_new_2) * uncer[i]) + np.sum(np.square(uncer[i + 1:i + N1])) + np.square(n_new_0 * uncer[i + N1])))
         n_new_2 = N - N1 + n_new_2
-        #print("SHAPE:", "SHOULD:", int(len(wvl)/N))
     return np.array(new_wvl), np.array(new_flux), np.array(new_uncer)
+
+
+def lowres_dataset(dataset, wbin):
+    dataset_value_list = list(dataset.values())
+    dataset_key_list = list(dataset.keys())
+    lowres_dataset_dict = {}
+    #bin_factor = rebin_spec(wbin)
+    sn = 0
+    while sn < len(dataset_value_list):
+        dataset_value_list[sn].rebin_spec(wbin)
+        lowres_dataset_dict[dataset_key_list[sn]] = dataset_value_list[sn]
+        lowres_dataset_dict.update(lowres_dataset_dict)
+        sn = sn + 1
+    return lowres_dataset_dict#, bin_factor
